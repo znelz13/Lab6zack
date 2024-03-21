aae8b3b664c5d332313b4718c83f6e944e94e307
znelz13 <znelz13@gmail.com>
2024-03-20 23:08:56 -0400

    Second edition

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Encoder that shifts all numbers up by 3
 def password_encoder(password):
     # Ensure the input is exactly 8 digits long
     if len(password) != 8 or not password.isdigit():
@@ -8,8 +9,7 @@ def password_encoder(password):
 
     return encoded_string
 
-
-# Define the function to perform the reverse digit shift and also return the encoded version
+# Decoder that shifts all numbers down by 3
 def password_decoder(encoded_password):
     # Ensure the input is exactly 8 digits long
     if len(encoded_password) != 8 or not encoded_password.isdigit():
@@ -19,6 +19,7 @@ def password_decoder(encoded_password):
     decoded_password = "".join([str((int(char) - 3) % 10) for char in encoded_password])
 
     return decoded_password
+
 def print_menu():
     while True:
         print("Menue")
@@ -30,7 +31,6 @@ def print_menu():
         # Get user selection
         menu_option = int(input("\nPlease enter an option: "))
 
-
         if menu_option == 1:
             #
             password = input("Please enter your password to encode: ")
@@ -41,10 +41,9 @@ def print_menu():
             decoded_password = password_decoder(encoded_password)
             print(f"The encoded password {encoded_password} is , and the original password is {decoded_password}.")
 
-
         elif menu_option == 3:
-
            break
+# Print menu
 def main():
     print_menu()
 
